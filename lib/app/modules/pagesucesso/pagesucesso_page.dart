274efb0743b29d4529f23c6aadbff51d0ebac3ba
abnersanlai <abnersanlai@gmail.com>
2022-05-15 01:17:38 -0300

    first

---
+++ lib/app/modules/pagesucesso/pagesucesso_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:desafio/app/modules/pagesucesso/pagesucesso_store.dart';
+import 'package:flutter/material.dart';
+
+class PagesucessoPage extends StatefulWidget {
+  final String title;
+  const PagesucessoPage({Key? key, this.title = 'PagesucessoPage'})
+      : super(key: key);
+  @override
+  PagesucessoPageState createState() => PagesucessoPageState();
+}
+
+class PagesucessoPageState extends State<PagesucessoPage> {
+  final PagesucessoStore store = Modular.get();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: const <Widget>[],
+      ),
+    );
+  }
+}
---
+++ lib/app/modules/pesquisa/pesquisa_store.dart
@@ -0,0 +1,38 @@
+import 'package:desafio/app/services/models/cep_model.dart';
+import 'package:desafio/app/services/repository/cep_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:mobx/mobx.dart';
+
+part 'pesquisa_store.g.dart';
+
+class PesquisaStore = _PesquisaStoreBase with _$PesquisaStore;
+
+abstract class _PesquisaStoreBase with Store {
+  @observable
+  TextEditingController cepController = TextEditingController();
+
+  @observable
+  bool animated = false;
+
+  @observable
+  late ObservableFuture<CepModel?> cepModel;
+
+  @action
+  Future buscarCepRepository() => cepModel =
+      ObservableFuture(CepRepository().buscarCep(cepController.text));
+
+  @action
+  Future pesquisaCep() async {
+    animated = true;
+
+    await buscarCepRepository();
+
+    if (cepModel.status == FutureStatus.fulfilled) {
+      print(cepModel.value?.location.coordinates.latitude);
+    }
+    animated = false;
+  }
+
+  @action
+  erroPage() {}
+}
